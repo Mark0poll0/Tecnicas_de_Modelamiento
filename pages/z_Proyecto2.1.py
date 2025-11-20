c90ee5796dacc897fc847f524e45e881d944550e
Mark0poll0 <quispemark000@gmail.com>
2025-11-20 04:05:38 -0500

    Correccion Nomnbres

--- pages/z_Proyecto2.1.py
+++ pages/z_Proyecto2.1.py
@@ -16,7 +16,7 @@ dash.register_page(__name__, path="/Proyecto2.1", name="PROYECTO 2.1")
 layout = html.Div([
     # ------------------------- IZQUIERDA -------------------------
     html.Div([
-        html.H2("Modelo SIR Modificado - ASIGNACION 2 - Rumor", className="title"),
+        html.H2("Modelo SIR  - ASIGNACION 2 (GRUPO 01)- Rumor", className="title"),
 
         html.Label("Población total (N):"),
         dcc.Input(id="sirN", type="number", value=275, className="input-field"),
